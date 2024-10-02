787e51781bc444c92dd81d4c9452be4511021c35
Vlasig <vlasig274@gmail.com>
2024-10-02 19:56:52 +0300

    Add files via upload

---
+++ module_9_7.py
@@ -0,0 +1,25 @@
+def is_prime(func):
+    def wrapper(*numbers):
+        number = func(*numbers)
+        prime = True
+        if number < 2:
+            for i in range(2, int(number ** 0.5) + 1):
+                if number % i == 0:
+                    prime = False
+                    print("Составное")
+            return number
+        else:
+            prime = True
+            print("Простое")
+        return number
+    return wrapper
+
+
+@is_prime
+def sum_three(a, b, c):
+    _sum = a + b + c
+    return _sum
+
+
+result = sum_three(2, 3, 6)
+print(result)
